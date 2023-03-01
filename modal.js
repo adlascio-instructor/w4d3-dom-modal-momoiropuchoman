e453a3e460f6f48f024432afd5712ddec036e755
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-03-01 13:07:01 -0800

    Complete the exercise

--- modal.js
+++ modal.js
@@ -1,9 +1,24 @@
 console.log("here");
 
 // select button element
+const buttonElement = document.getElementById("openModal")
 
 // add event listener on click to button
-// remove class invisible from div modal
+buttonElement.addEventListener("click", () => {
+    // remove class invisible from div modal
+    const modalElement = document.getElementById("modal")
+    modalElement.classList.remove("invisible")
+  });
+
 
 // add event listener on key press Escape
-// add class invisible to div modal
+document.body.addEventListener("keydown", (event) => {
+    // add class invisible to div modal
+    if(event.key == "Escape") {
+        const modalElement = document.getElementById("modal")
+        modalElement.classList.add("invisible")
+    }
+  });
+
+
+
